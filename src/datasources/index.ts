6c935011aa75d26d43f3f8eb57893b51671d3d04
Garcez <allan.f.garcez@gmail.com>
2018-11-17 17:34:33 -0200

    Concat dataSources in Index

---
+++ src/datasources/index.ts
@@ -0,0 +1,9 @@
+import Country from './Country';
+import President from './President';
+import Cep from './Cep';
+
+export default {
+  country: new Country(),
+  president: new President(),
+  cep: new Cep(),
+};
