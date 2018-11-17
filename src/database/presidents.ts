292b852b337774c7b36d78e5f047e02e1c7e9d63
Garcez <allan.f.garcez@gmail.com>
2018-11-17 14:41:44 -0200

    Emulate Database

---
+++ src/database/presidents.ts
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    age: 30,
+    name: 'Fuckerson',
+    country: {
+      name: 'American Samoa',
+    },
+  },
+  {
+    id: 2,
+    age: 40,
+    name: 'Nelson',
+    country: {
+      name: 'American Samoa',
+    },
+  },
+  {
+    id: 3,
+    age: 30,
+    name: 'Fulano',
+    country: {
+      name: 'American Samoa',
+    },
+  },
+  {
+    id: 4,
+    age: 30,
+    name: 'Beltrano',
+    country: {
+      name: 'American Samoa',
+    },
+  },
+  {
+    name: 'Ciclano',
+    id: 5,
+    age: 30,
+    country: {
+      name: 'American Samoa',
+    },
+  },
+  {
+    id: 6,
+    age: 63,
+    name: 'Bolsonaro',
+    country: {
+      name: 'Zimbabwe',
+    },
+  },
+];
