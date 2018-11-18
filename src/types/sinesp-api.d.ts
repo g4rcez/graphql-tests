f8907ee48093e82ddd2d00b057ce5005ce0850ac
Garcez <allan.f.garcez@gmail.com>
2018-11-18 01:57:16 -0200

    Automatic generated .d.ts

---
+++ src/types/sinesp-api.d.ts
@@ -0,0 +1,11 @@
+export function configure({
+  host,
+  serviceVersion,
+  androidVersion,
+  endpoint,
+  secret,
+  timeout,
+  maximumRetry,
+  proxy = {},
+}: any): any;
+export function search(plate: any): any;
