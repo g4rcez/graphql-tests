0b18cbe17d4896e1e6a12a93df998e455a5fa06d
Garcez <allan.f.garcez@gmail.com>
2018-11-18 01:55:33 -0200

    Add plate property on dataSources

--- src/datasources/index.ts
+++ src/datasources/index.ts
@@ -1,9 +1,11 @@
 import Country from './Country';
 import President from './President';
 import Cep from './Cep';
+import Plate from './Plate';
 
 export default {
   country: new Country(),
   president: new President(),
   cep: new Cep(),
+  plate: new Plate()
 };
