2446013075d651d62324c647e3eb38cd63bd240b
g4rcez <allan.f.garcez@gmail.com>
2020-04-13 16:50:45 -0300

    refactor: new types and fix old types

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,20 @@
 import { DataSource } from "./data-sources";
 
 export type AnyArg = {
-  [key: string]: string | number;
+    [key: string]: string | number;
 };
 export type Args<T> = {
-  [key in keyof T]: T[key];
+    [key in keyof T]: T[key];
 };
 
 type Context = {
-  dataSources: DataSource;
+    dataSources: DataSource;
 };
-export type Resolver<
-  Parent extends unknown,
-  Arguments extends AnyArg,
-  Model extends any
-> = (
-  parent: Parent,
-  args: Args<Arguments>,
-  context: Context,
-  info: unknown
+export type Resolver<Parent extends unknown, Arguments extends AnyArg, Model extends any> = (
+    parent: Parent,
+    args: Args<Arguments>,
+    context: Context,
+    info: unknown
 ) => Model;
+
+export type ID = string;
