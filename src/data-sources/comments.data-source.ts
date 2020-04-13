5baadac12f1419199d297c875c90232ab6c70871
g4rcez <allan.f.garcez@gmail.com>
2020-04-13 16:55:31 -0300

    fix: rename datasources to new convention

--- src/data-sources/comments.data-source.ts
+++ src/data-sources/comments.data-source.ts
@@ -1,25 +1,26 @@
 import { RESTDataSource } from "apollo-datasource-rest";
+import { Comment } from "../schemas";
 
 export default class CommentDataSource extends RESTDataSource {
-  public constructor() {
-    super();
-    this.baseURL = "http://localhost:3000/comments";
-  }
+    public constructor() {
+        super();
+        this.baseURL = "http://localhost:3000/comments";
+    }
 
-  public async getComment(id: string | number) {
-    const result = await this.get(`/${id}`);
-    return this.resolveComment(result);
-  }
+    public async getComment(id: string | number): Promise<Comment> {
+        const result = await this.get(`/${id}`);
+        return this.resolveComment(result);
+    }
 
-  public async getComments() {
-    const list = await this.get("/");
-    return list.map((x: any) => this.resolveComment(x));
-  }
+    public async getComments(): Promise<Comment[]> {
+        const list = await this.get("/");
+        return list.map((x: Comment) => this.resolveComment(x));
+    }
 
-  private async resolveComment(comment: any) {
-    return {
-      ...comment,
-      post: this.context.dataSources.postDataSource.getPost(comment.postId),
-    };
-  }
+    private async resolveComment(comment: any): Promise<Comment> {
+        return {
+            ...comment,
+            post: this.context.dataSources.postDataSource.getPost(comment.postId)
+        };
+    }
 }
--- src/data-sources/posts.data-source.ts
+++ src/data-sources/posts.data-source.ts
@@ -1,16 +1,17 @@
 import { RESTDataSource } from "apollo-datasource-rest";
+import { Post } from "../schemas";
 
 export default class PostDataSource extends RESTDataSource {
-  public constructor() {
-    super();
-    this.baseURL = "http://localhost:3000/posts";
-  }
+    public constructor() {
+        super();
+        this.baseURL = "http://localhost:3000/posts";
+    }
 
-  public async getPost(id: string | number) {
-    return this.get(`/${id}`);
-  }
+    public async getPost(id: string | number): Promise<Post> {
+        return this.get(`/${id}`);
+    }
 
-  public async getPosts() {
-    return this.get("/");
-  }
+    public async getPosts(): Promise<Post[]> {
+        return this.get("/");
+    }
 }
